4998bb7b173c221d7c340c290ba1818d392a9d10
maamir27 <97367606+maamir27@users.noreply.github.com>
2023-11-18 17:04:42 -0500

    controllers and stations default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_18_210635) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_18_220238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_18_210635) do
     t.float "longitude"
     t.integer "avg_rating"
     t.datetime "last_updated", precision: nil
-    t.boolean "status"
+    t.boolean "status", default: true
     t.bigint "review_id", null: false
     t.index ["review_id"], name: "index_stations_on_review_id"
   end

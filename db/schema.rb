85674c39f42ed3fdd504d79348f1f80cab8723fc
maamir27 <97367606+maamir27@users.noreply.github.com>
2023-11-18 16:03:26 -0500

    foreign keys added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_18_205518) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_18_210112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,9 +55,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_18_205518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.bigint "score_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["score_id"], name: "index_users_on_score_id"
   end
 
   add_foreign_key "scores", "users"
+  add_foreign_key "users", "scores"
 end

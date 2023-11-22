7fb048187febd3cae76237b929db89020d5d0193
maamir27 <97367606+maamir27@users.noreply.github.com>
2023-11-21 20:19:00 -0500

    rename column 'intersection' to 'address'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_18_221615) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_011741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,7 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_18_221615) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "intersection"
+    t.string "address"
     t.float "latitude"
     t.float "longitude"
     t.integer "avg_rating"

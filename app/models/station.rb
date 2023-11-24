f3c51c81c941260e3681eb16be834ab40dc8dc10
maamir27 <97367606+maamir27@users.noreply.github.com>
2023-11-23 19:04:38 -0500

    migration to stations table to connect user_id

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,6 @@
 class Station < ApplicationRecord
   has_many :reviews
+  belongs_to :user
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_020643) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_000334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,7 +78,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_020643) do
     t.boolean "status", default: true
     t.bigint "review_id", null: false
     t.string "description"
+    t.bigint "user_id", null: false
     t.index ["review_id"], name: "index_stations_on_review_id"
+    t.index ["user_id"], name: "index_stations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -100,4 +102,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_020643) do
   add_foreign_key "reviews", "users"
   add_foreign_key "scores", "users"
   add_foreign_key "stations", "reviews"
+  add_foreign_key "stations", "users"
 end

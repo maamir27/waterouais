7ca1ba1b764e41c4a3abc9678828018698f9429f
jaden <nguyenthucanh422@gmail.com>
2023-11-28 18:14:25 -0500

    commented out styled search bar and changed body font due to line height issue

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -15,15 +15,15 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
-  })
+    })
 
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
 
-    this.map.addControl(new MapboxGeocoder({
+    this.map.addControl(new MapboxGeocoder( {
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl }))
-    }
+  }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
@@ -34,7 +34,8 @@ export default class extends Controller {
           .addTo(this.map);
     })
   }
-    #fitMapToMarkers() {
+
+  #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })

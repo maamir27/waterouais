2b13b7e54037646793ea5f1de749a7534e98331d
Maelle Rudner Mercier <mazz.r.m@gmail.com>
2023-11-28 21:10:40 -0500

    user profile including user controller and rank

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+
+    @user_rank = User.joins(:score)
+    .where('scores.total_score > ?', @user.score.total_score)
+    .count + 1
+
+    @reviews_submitted = @user.reviews
+
+    @created_stations = @user.stations
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end

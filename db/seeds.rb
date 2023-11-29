b3612408642f08222931229efe58206b4bef335a
maamir27 <97367606+maamir27@users.noreply.github.com>
2023-11-28 20:59:43 -0500

    seed file with one station

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
+
+img = URI.open("https://imtl.org/image/big/1_MG_0539.jpg")
+
+user = User.create!({
+    email: 'water@waterouais.com',
+    password: 'waterisw3t',
+    password_confirmation: 'waterisw3t'
+})
+
+station = Station.create!({
+  name: 'Jean Talon Market',
+  address: '7070 Av Henri Julien, Montreal, Quebec',
+  description: 'Turn left at the turnstiles',
+  user: user
+});
+station.photos.attach(io: img, filename: "station.jpg", content_type: "image/jpg")
+station.save

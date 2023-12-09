5d46fd4823edda585e63200abb2a116db8526347
maamir27 <97367606+maamir27@users.noreply.github.com>
2023-12-09 15:52:06 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,31 +9,54 @@
 #   end
 require "open-uri"
 
-img = URI.open("https://imtl.org/image/big/1_MG_0539.jpg")
-img2 = URI.open("https://westmount.org/wp-content/uploads/2020/07/waterdispenser-800x445.jpg")
+# USERS
+jaden_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U05FVJE3E83-fc0a168c87c5-512")
+mazz_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U05FX4MEW04-08bec44f1d05-512")
+malik_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U04H840LTJB-23655dd3fa99-512")
 
-user = User.create!({
-    username: 'maamir',
-    email: 'water@waterouais.com',
-    password: 'waterisw3t',
-    password_confirmation: 'waterisw3t'
+mazz = User.create!({
+    username: 'mazz',
+    email: 'mazz@waterouais.com',
+    password: 'mazz321',
+    password_confirmation: 'mazz321'
 })
+mazz.avatar.attach(io: mazz_avatar, filename: "mazz_avatar.jpg", content_type: "image/jpg")
+mazz.save
 
-station = Station.create!({
-  name: 'Jean Talon Market',
+jaden = User.create!( {
+    username: 'jaden',
+    email: 'jaden@waterouais.com',
+    password: 'jaden321',
+    password_confirmation: 'jaden321'
+})
+jaden.avatar.attach(io: jaden_avatar, filename: "jaden_avatar.jpeg", content_type: "image/jpeg")
+jaden.save
+
+malik = User.create!({
+    username: 'malik',
+    email: 'malik@waterouais.com',
+    password: 'malik321',
+    password_confirmation: 'malik321'
+})
+malik.avatar.attach(io: malik_avatar, filename: "malik_avatar.jpg", content_type: "image/jpg")
+malik.save
+
+jean_talon = Station.create!({
+  name: 'Jean-Talon Market',
   address: '7070 Av Henri Julien, Montreal, Quebec',
   description: 'Turn left at the turnstiles',
-  user: user
+  user: malik
 });
 
 westmount = Station.create!({
   name: 'Westmount Park',
   address: '327 Av. Melville, Westmount, Quebec',
   description: 'Really cool handsfree automatic water dispenser!',
-  user: user
+  user: mazz
 })
 
-station.photos.attach(io: img, filename: "station.jpg", content_type: "image/jpg")
-station.save
-westmount.photos.attach(io: img2, filename: "westmount.jpg", content_type: "image/jpg")
-westmount.save
+
+# station.photos.attach(io: img, filename: "station.jpg", content_type: "image/jpg")
+# station.save
+# westmount.photos.attach(io: img2, filename: "westmount.jpg", content_type: "image/jpg")
+# westmount.save
